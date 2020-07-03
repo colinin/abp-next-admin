8afad1fd213d86e526b08feb4b9e9927e377e333
cKey <35512826+colinin@users.noreply.github.com>
2020-07-03 09:48:01 +0800

    add inter-service invocation interface authorization cache

---
+++ aspnet-core/modules/common/LINGYUN.Abp.IdentityModel/LINGYUN/Abp/IdentityModel/AbpCachedIdentityModelModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.IdentityModel;
+using Volo.Abp.Modularity;
+using Volo.Abp.Security;
+
+namespace LINGYUN.Abp.IdentityModel
+{
+    [DependsOn(
+        typeof(AbpIdentityModelModule),
+        typeof(AbpSecurityModule))]
+    public class AbpCachedIdentityModelModule : AbpModule
+    {
+    }
+}
