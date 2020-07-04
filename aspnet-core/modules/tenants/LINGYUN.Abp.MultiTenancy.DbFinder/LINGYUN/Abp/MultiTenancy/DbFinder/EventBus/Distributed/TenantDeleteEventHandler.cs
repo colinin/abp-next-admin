00382e9d4ed122a44927e10b6ad7d302d5ae5cd6
cKey <35512826+colinin@users.noreply.github.com>
2020-07-04 09:11:23 +0800

    Increase event subscriptions for tenants

---
+++ aspnet-core/modules/tenants/LINGYUN.Abp.MultiTenancy.DbFinder/LINGYUN/Abp/MultiTenancy/DbFinder/EventBus/Distributed/TenantDeleteEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events.Distributed;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.TenantManagement;
+
+namespace LINGYUN.Abp.MultiTenancy.DbFinder.EventBus.Distributed
+{
+    public class TenantDeleteEventHandler : IDistributedEventHandler<EntityDeletedEto<TenantEto>>, ITransientDependency
+    {
+        private readonly IDistributedCache<TenantConfigurationCacheItem> _cache;
+
+        public TenantDeleteEventHandler(
+            IDistributedCache<TenantConfigurationCacheItem> cache)
+        {
+            _cache = cache;
+        }
+
+        public virtual async Task HandleEventAsync(EntityDeletedEto<TenantEto> eventData)
+        {
+            var cacheKey = TenantConfigurationCacheItem.CalculateCacheKey(eventData.Entity.Id.ToString());
+            await _cache.RemoveAsync(cacheKey);
+        }
+    }
+}
---
+++ aspnet-core/modules/tenants/LINGYUN.Abp.MultiTenancy.DbFinder/LINGYUN/Abp/MultiTenancy/DbFinder/EventBus/Distributed/TenantUpdateEventHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events.Distributed;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.TenantManagement;
+
+namespace LINGYUN.Abp.MultiTenancy.DbFinder.EventBus.Distributed
+{
+    public class TenantUpdateEventHandler : IDistributedEventHandler<EntityUpdatedEto<TenantEto>>, ITransientDependency
+    {
+        private readonly IDataFilter _dataFilter;
+        private readonly ITenantRepository _tenantRepository;
+        private readonly IDistributedCache<TenantConfigurationCacheItem> _cache;
+
+        public TenantUpdateEventHandler(
+            IDataFilter dataFilter,
+            ITenantRepository tenantRepository,
+            IDistributedCache<TenantConfigurationCacheItem> cache)
+        {
+            _cache = cache;
+            _dataFilter = dataFilter;
+            _tenantRepository = tenantRepository;
+        }
+
+        public virtual async Task HandleEventAsync(EntityUpdatedEto<TenantEto> eventData)
+        {
+            // 禁用租户过滤器
+            using (_dataFilter.Disable<IMultiTenant>())
+            {
+                var tenant = await _tenantRepository.FindAsync(eventData.Entity.Id, true);
+                if (tenant == null)
+                {
+                    return;
+                }
+                var connectionStrings = new ConnectionStrings();
+                foreach (var tenantConnectionString in tenant.ConnectionStrings)
+                {
+                    connectionStrings[tenantConnectionString.Name] = tenantConnectionString.Value;
+                }
+                var cacheItem = new TenantConfigurationCacheItem(tenant.Id, tenant.Name, connectionStrings);
+
+                var cacheKey = TenantConfigurationCacheItem.CalculateCacheKey(eventData.Entity.Id.ToString());
+                await _cache.SetAsync(cacheKey, cacheItem);
+            }
+        }
+    }
+}
