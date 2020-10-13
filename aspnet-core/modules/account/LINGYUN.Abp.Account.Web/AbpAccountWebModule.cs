c638462e9dd54fa239cb3e959428bb5e4a8c270f
cKey <35512826+colinin@users.noreply.github.com>
2020-10-13 13:49:39 +0800

    upgrade auth-server packages

---
+++ aspnet-core/modules/account/LINGYUN.Abp.Account.Web/AbpAccountWebModule.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Modularity;
+
+namespace LINGYUN.Abp.Account.Web
+{
+    [DependsOn(typeof(Volo.Abp.Account.Web.AbpAccountWebModule))]
+    public class AbpAccountWebModule : AbpModule
+    {
+    }
+}
