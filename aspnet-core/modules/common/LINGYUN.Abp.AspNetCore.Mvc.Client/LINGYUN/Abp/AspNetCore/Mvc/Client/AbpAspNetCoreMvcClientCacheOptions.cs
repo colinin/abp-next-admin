3ffb26772bcfab82550d8f592d719fb234f6ded7
cKey <35512826+colinin@users.noreply.github.com>
2021-03-23 17:08:19 +0800

    add modules LINGYUN.Abp.AspNetCore.Mvc.Client

---
+++ aspnet-core/modules/common/LINGYUN.Abp.AspNetCore.Mvc.Client/LINGYUN/Abp/AspNetCore/Mvc/Client/AbpAspNetCoreMvcClientCacheOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace LINGYUN.Abp.AspNetCore.Mvc.Client
+{
+    public class AbpAspNetCoreMvcClientCacheOptions
+    {
+        /// <summary>
+        /// 用户缓存过期时间, 单位: 秒
+        /// 默认: 300
+        /// </summary>
+        public int UserCacheExpirationSeconds { get; set; } = 300;
+        /// <summary>
+        /// 匿名用户缓存过期时间, 单位: 秒
+        /// 默认: 300
+        /// </summary>
+        public int AnonymousCacheExpirationSeconds { get; set; } = 300;
+    }
+}
---
+++ aspnet-core/modules/common/LINGYUN.Abp.AspNetCore.Mvc.Client/LINGYUN/Abp/AspNetCore/Mvc/Client/AbpAspNetCoreMvcClientModule.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Volo.Abp.AspNetCore.Mvc.Client;
+using Volo.Abp.EventBus;
+using Volo.Abp.Modularity;
+
+namespace LINGYUN.Abp.AspNetCore.Mvc.Client
+{
+    [DependsOn(
+       typeof(AbpAspNetCoreMvcClientCommonModule),
+       typeof(AbpEventBusModule)
+       )]
+    public class AbpAspNetCoreMvcClientModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            Configure<AbpAspNetCoreMvcClientCacheOptions>(configuration.GetSection("AbpMvcClient:Cache"));
+        }
+    }
+}
