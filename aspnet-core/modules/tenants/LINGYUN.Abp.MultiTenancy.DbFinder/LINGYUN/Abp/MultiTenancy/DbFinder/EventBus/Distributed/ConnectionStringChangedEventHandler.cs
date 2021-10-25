93dc0369cc1653249098772a8e51f636d7d2fcc2
cKey <35512826+colinin@users.noreply.github.com>
2021-10-25 10:38:10 +0800

    fix(solution): add files that were accidentally ignored

---
+++ aspnet-core/modules/tenants/LINGYUN.Abp.MultiTenancy.DbFinder/LINGYUN/Abp/MultiTenancy/DbFinder/EventBus/Distributed/ConnectionStringChangedEventHandler.cs
@@ -0,0 +1,68 @@
+﻿using LINGYUN.Abp.MultiTenancy.DbFinder;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.TenantManagement;
+using Volo.Abp.Uow;
+
+namespace LINGYUN.Abp.MultiTenancy.RemoteService.EventBus.Distributed
+{
+    public class ConnectionStringChangedEventHandler : IDistributedEventHandler<ConnectionStringChangedEventData>, ITransientDependency
+    {
+        private readonly ILogger<ConnectionStringChangedEventHandler> _logger;
+        private readonly ICurrentTenant _currentTenant;
+        private readonly ITenantRepository _tenantRepository;
+        private readonly IDistributedCache<TenantConfigurationCacheItem> _cache;
+
+        public ConnectionStringChangedEventHandler(
+             ICurrentTenant currentTenant,
+             ITenantRepository tenantRepository,
+             ILogger<ConnectionStringChangedEventHandler> logger,
+             IDistributedCache<TenantConfigurationCacheItem> cache)
+        {
+            _cache = cache;
+            _logger = logger;
+            _currentTenant = currentTenant;
+            _tenantRepository = tenantRepository;
+        }
+
+        [UnitOfWork]
+        public virtual async Task HandleEventAsync(ConnectionStringChangedEventData eventData)
+        {
+            try
+            {
+                using (_currentTenant.Change(null))
+                {
+                    var tenant = await _tenantRepository.FindAsync(eventData.Id, true);
+                    if (tenant == null)
+                    {
+                        return;
+                    }
+                    var connectionStrings = new ConnectionStrings();
+                    foreach (var tenantConnectionString in tenant.ConnectionStrings)
+                    {
+                        connectionStrings[tenantConnectionString.Name] = tenantConnectionString.Value;
+                    }
+                    var cacheItem = new TenantConfigurationCacheItem(tenant.Id, tenant.Name, connectionStrings);
+
+                    await _cache.SetAsync(
+                        TenantConfigurationCacheItem.CalculateCacheKey(eventData.Id.ToString()),
+                        cacheItem);
+
+                    await _cache.SetAsync(
+                        TenantConfigurationCacheItem.CalculateCacheKey(eventData.Name),
+                        cacheItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogException(ex);
+            }
+        }
+    }
+}
---
+++ aspnet-core/modules/tenants/LINGYUN.Abp.MultiTenancy.RemoteService/LINGYUN/Abp/MultiTenancy/RemoteService/EventBus/Distributed/ConnectionStringChangedEventHandler.cs
@@ -0,0 +1,44 @@
+﻿using LINGYUN.Abp.TenantManagement;
+using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+
+namespace LINGYUN.Abp.MultiTenancy.RemoteService.EventBus.Distributed
+{
+    public class ConnectionStringChangedEventHandler : IDistributedEventHandler<ConnectionStringChangedEventData>, ITransientDependency
+    {
+        private readonly ITenantAppService _tenantAppService;
+        private readonly IDistributedCache<TenantConfigurationCacheItem> _cache;
+
+        public ConnectionStringChangedEventHandler(
+            ITenantAppService tenantAppService,
+            IDistributedCache<TenantConfigurationCacheItem> cache)
+        {
+            _cache = cache;
+            _tenantAppService = tenantAppService;
+        }
+
+
+        public virtual async Task HandleEventAsync(ConnectionStringChangedEventData eventData)
+        {
+            var tenantDto = await _tenantAppService.GetAsync(eventData.Id);
+            var tenantConnectionStringsDto = await _tenantAppService.GetConnectionStringAsync(eventData.Id);
+            var connectionStrings = new ConnectionStrings();
+            foreach (var tenantConnectionString in tenantConnectionStringsDto.Items)
+            {
+                connectionStrings[tenantConnectionString.Name] = tenantConnectionString.Value;
+            }
+            var cacheItem = new TenantConfigurationCacheItem(tenantDto.Id, tenantDto.Name, connectionStrings);
+
+            await _cache.SetAsync(
+                TenantConfigurationCacheItem.CalculateCacheKey(eventData.Id.ToString()), 
+                cacheItem);
+
+            await _cache.SetAsync(
+                TenantConfigurationCacheItem.CalculateCacheKey(tenantDto.Name),
+                cacheItem);
+        }
+    }
+}
