8c39a153790d4e02df597c3be8879e5db6a4c6a3
cKey <35512826+colinin@users.noreply.github.com>
2021-11-28 18:51:26 +0800

    feat(dapr): 使dapr代理请求过程可配置

--- aspnet-core/modules/dapr/LINGYUN.Abp.Dapr.Client/LINGYUN/Abp/Dapr/Client/DynamicProxying/AbpDaprClientProxyOptions.cs
+++ aspnet-core/modules/dapr/LINGYUN.Abp.Dapr.Client/LINGYUN/Abp/Dapr/Client/DynamicProxying/AbpDaprClientProxyOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace LINGYUN.Abp.Dapr.Client.DynamicProxying
 {
@@ -7,9 +8,17 @@ namespace LINGYUN.Abp.Dapr.Client.DynamicProxying
     {
         public Dictionary<Type, DynamicDaprClientProxyConfig> DaprClientProxies { get; set; }
 
+        /// <summary>
+        /// 增加一个可配置的请求消息
+        /// 参数一:    appId
+        /// 参数二:    HttpRequestMessage
+        /// </summary>
+        public List<Action<string, HttpRequestMessage>> ProxyRequestActions { get; }
+
         public AbpDaprClientProxyOptions()
         {
             DaprClientProxies = new Dictionary<Type, DynamicDaprClientProxyConfig>();
+            ProxyRequestActions = new List<Action<string, HttpRequestMessage>>();
         }
     }
 }
