92702708996c61dd6e18b54a3df206d98a77c9bb
cKey <35512826+colinin@users.noreply.github.com>
2021-12-09 21:11:26 +0800

    fix: add missing folder

---
+++ aspnet-core/services/LY.MicroService.identityServer.HttpApi.Host/Emailing/Templates/IdentityEmailTemplates.cs
@@ -0,0 +1,7 @@
+﻿namespace LY.MicroService.IdentityServer.Emailing.Templates
+{
+    public static class IdentityEmailTemplates
+    {
+        public const string EmailConfirmed = "Abp.Identity.EmailConfirmed";
+    }
+}
