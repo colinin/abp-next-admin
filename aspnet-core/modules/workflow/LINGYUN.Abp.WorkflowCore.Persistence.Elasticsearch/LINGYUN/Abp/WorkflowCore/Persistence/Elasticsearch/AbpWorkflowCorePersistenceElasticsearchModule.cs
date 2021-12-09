a11a2dee5ab260f49cd6f6559109e63d7d42071b
cKey <35512826+colinin@users.noreply.github.com>
2021-12-09 14:53:54 +0800

    fix(workflow): add missing module dependencies

--- aspnet-core/modules/workflow/LINGYUN.Abp.WorkflowCore.Persistence.Elasticsearch/LINGYUN/Abp/WorkflowCore/Persistence/Elasticsearch/AbpWorkflowCorePersistenceElasticsearchModule.cs
+++ aspnet-core/modules/workflow/LINGYUN.Abp.WorkflowCore.Persistence.Elasticsearch/LINGYUN/Abp/WorkflowCore/Persistence/Elasticsearch/AbpWorkflowCorePersistenceElasticsearchModule.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using LINGYUN.Abp.Elasticsearch;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Modularity;
 using WorkflowCore.Interface;
 using WorkflowCore.Models;
@@ -6,6 +7,7 @@ using WorkflowCore.Models;
 namespace LINGYUN.Abp.WorkflowCore.Persistence.Elasticsearch
 {
     [DependsOn(typeof(AbpWorkflowCoreModule))]
+    [DependsOn(typeof(AbpElasticsearchModule))]
     public class AbpWorkflowCorePersistenceElasticsearchModule : AbpModule
     {
         public override void PreConfigureServices(ServiceConfigurationContext context)
