1e2c72eb0f79eb43f5ef5e9f166dec2b97cdcc85
cKey <35512826+colinin@users.noreply.github.com>
2022-06-25 16:26:57 +0800

    feat: add missing localizer text

--- aspnet-core/modules/text-templating/LINGYUN.Abp.TextTemplating.Application.Contracts/LINGYUN/Abp/TextTemplating/Permissions/AbpTextTemplatingPermissionDefinitionProvider.cs
+++ aspnet-core/modules/text-templating/LINGYUN.Abp.TextTemplating.Application.Contracts/LINGYUN/Abp/TextTemplating/Permissions/AbpTextTemplatingPermissionDefinitionProvider.cs
@@ -8,9 +8,9 @@ public class AbpTextTemplatingPermissionDefinitionProvider : PermissionDefinitio
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var saasGroup = context.AddGroup(AbpTextTemplatingPermissions.GroupName, L("Permission:Saas"));
+        var textTemplatingGroup = context.AddGroup(AbpTextTemplatingPermissions.GroupName, L("Permission:TextTemplating"));
 
-        var textTemplatePermission = saasGroup.AddPermission(AbpTextTemplatingPermissions.TextTemplate.Default, L("Permission:TextTemplates"));
+        var textTemplatePermission = textTemplatingGroup.AddPermission(AbpTextTemplatingPermissions.TextTemplate.Default, L("Permission:TextTemplates"));
         textTemplatePermission.AddChild(AbpTextTemplatingPermissions.TextTemplate.Create, L("Permission:Create"));
         textTemplatePermission.AddChild(AbpTextTemplatingPermissions.TextTemplate.Update, L("Permission:Edit"));
         textTemplatePermission.AddChild(AbpTextTemplatingPermissions.TextTemplate.Delete, L("Permission:Delete"));
