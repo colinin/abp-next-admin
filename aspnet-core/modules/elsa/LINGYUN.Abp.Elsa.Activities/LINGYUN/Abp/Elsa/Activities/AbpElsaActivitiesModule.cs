83a486971bec88989e4c892a89138f15fdb087f9
cKey <35512826+colinin@users.noreply.github.com>
2022-08-01 14:58:06 +0800

    add emailing activities

---
+++ aspnet-core/modules/elsa/LINGYUN.Abp.Elsa.Activities/LINGYUN/Abp/Elsa/Activities/AbpElsaActivitiesModule.cs
@@ -0,0 +1,40 @@
+﻿using Elsa;
+using Elsa.Options;
+using LINGYUN.Abp.Elsa.Activities.BlobStoring;
+using LINGYUN.Abp.Elsa.Activities.Emailing;
+using LINGYUN.Abp.Elsa.Activities.IM;
+using LINGYUN.Abp.Elsa.Activities.Notifications;
+using LINGYUN.Abp.Elsa.Activities.Sms;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace LINGYUN.Abp.Elsa.Activities;
+
+[DependsOn(
+    typeof(AbpElsaModule),
+    typeof(AbpElsaActivitiesBlobStoringModule),
+    typeof(AbpElsaActivitiesEmailingModule),
+    typeof(AbpElsaActivitiesIMModule),
+    typeof(AbpElsaActivitiesNotificationsModule),
+    typeof(AbpElsaActivitiesSmsModule))]
+public class AbpElsaActivitiesModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        var elsaSection = configuration.GetSection("Elsa");
+        var startups = new[]
+        {
+            typeof(Emailing.Startup),
+            typeof(BlobStoring.Startup),
+            typeof(Notifications.Startup),
+            typeof(Sms.Startup),
+            typeof(IM.Startup),
+        };
+
+        PreConfigure<ElsaOptionsBuilder>(elsa =>
+        {
+            elsa.AddFeatures(startups, configuration);
+        });
+    }
+}
