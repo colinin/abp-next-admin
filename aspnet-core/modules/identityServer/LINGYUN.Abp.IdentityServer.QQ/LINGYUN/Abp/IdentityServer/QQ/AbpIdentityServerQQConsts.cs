ba4c8cb15629aa88b5dcfa986bc0cf8f05b49b05
cKey <35512826+colinin@users.noreply.github.com>
2022-10-06 11:39:36 +0800

    remove useless modules

--- aspnet-core/modules/identityServer/LINGYUN.Abp.IdentityServer.QQ/LINGYUN/Abp/IdentityServer/QQ/AbpIdentityServerQQConsts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LINGYUN.Abp.IdentityServer.QQ;
-
-public static class AbpIdentityServerQQConsts
-{
-    public static string AuthenticationScheme { get; set; } = "QQ Connect";
-    public static string DisplayName { get; set; } = "QQ Connect";
-    public static string CallbackPath { get; set; } = "/signin-qq";
-}
