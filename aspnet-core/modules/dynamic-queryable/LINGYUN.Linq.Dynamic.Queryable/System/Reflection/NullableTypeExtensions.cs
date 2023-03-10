7018f6e90ca10f6e9ec37d45a2d3746c7c302f77
cKey <35512826+colinin@users.noreply.github.com>
2023-03-10 20:43:14 +0800

    fix(type): fix nullable-type-match

--- aspnet-core/modules/dynamic-queryable/LINGYUN.Linq.Dynamic.Queryable/System/Reflection/NullableTypeExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System.Reflection;
-
-public static class NullableTypeExtensions
-{
-    public static bool IsNullableType(this Type theType)
-    {
-        return (theType.IsGenericType && theType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)));
-    }
-}
