b43e87684e674dbed7a66d6ab02006c43eb0802a
cKey <35512826+colinin@users.noreply.github.com>
2023-03-30 11:02:44 +0800

    feat(tasks): change job group length

--- aspnet-core/migrations/LY.MicroService.TaskManagement.EntityFrameworkCore/Migrations/20230330025320_Modify-Job-Group-Length.Designer.cs
+++ aspnet-core/migrations/LY.MicroService.TaskManagement.EntityFrameworkCore/Migrations/20230330025320_Modify-Job-Group-Length.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LY.MicroService.TaskManagement.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace LY.MicroService.TaskManagement.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(TaskManagementMigrationsDbContext))]
-    partial class TaskManagementMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230330025320_Modify-Job-Group-Length")]
+    partial class ModifyJobGroupLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
