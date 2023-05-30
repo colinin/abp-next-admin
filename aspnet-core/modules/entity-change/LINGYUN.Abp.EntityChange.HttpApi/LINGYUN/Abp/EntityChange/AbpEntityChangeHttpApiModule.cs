75110de7892eb1d0d087a7ec918a20c6906a1831
colin <colin.in@foxmail.com>
2023-05-30 13:37:28 +0800

    add entity-change manage

---
+++ aspnet-core/modules/entity-change/LINGYUN.Abp.EntityChange.HttpApi/LINGYUN/Abp/EntityChange/AbpEntityChangeHttpApiModule.cs
@@ -0,0 +1,26 @@
+﻿using LINGYUN.Abp.EntityChange.Localization;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.Localization;
+using Volo.Abp.Modularity;
+
+namespace LINGYUN.Abp.EntityChange;
+
+[DependsOn(
+    typeof(AbpEntityChangeApplicationContractsModule),
+    typeof(AbpAspNetCoreMvcModule))]
+public class AbpEntityChangeHttpApiModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        PreConfigure<IMvcBuilder>(mvcBuilder =>
+        {
+            mvcBuilder.AddApplicationPartIfNotExists(typeof(AbpEntityChangeHttpApiModule).Assembly);
+        });
+
+        PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
+        {
+            options.AddAssemblyResource(typeof(AbpEntityChangeResource), typeof(AbpEntityChangeApplicationContractsModule).Assembly);
+        });
+    }
+}
