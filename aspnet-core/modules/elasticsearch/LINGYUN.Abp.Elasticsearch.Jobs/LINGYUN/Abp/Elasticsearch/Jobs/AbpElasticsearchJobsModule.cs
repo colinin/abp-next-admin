1aa76432419abc60bc7b7d5b026db741fe5bc4f0
colin <colin.in@foxmail.com>
2023-06-25 09:27:03 +0800

    feat(jobs): add elasticsearch jobs.

---
+++ aspnet-core/modules/elasticsearch/LINGYUN.Abp.Elasticsearch.Jobs/LINGYUN/Abp/Elasticsearch/Jobs/AbpElasticsearchJobsModule.cs
@@ -0,0 +1,29 @@
+﻿using LINGYUN.Abp.BackgroundTasks;
+using LINGYUN.Abp.Elasticsearch.Jobs.Localization;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.Timing;
+using Volo.Abp.VirtualFileSystem;
+
+namespace LINGYUN.Abp.Elasticsearch.Jobs;
+
+[DependsOn(typeof(AbpTimingModule))]
+[DependsOn(typeof(AbpElasticsearchModule))]
+[DependsOn(typeof(AbpBackgroundTasksAbstractionsModule))]
+public class AbpElasticsearchJobsModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpElasticsearchJobsModule>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Add<ElasticsearchJobsResource>()
+                .AddVirtualJson("/LINGYUN/Abp/Elasticsearch/Jobs/Localization/Resources");
+        });
+    }
+}
---
+++ aspnet-core/modules/elasticsearch/LINGYUN.Abp.Elasticsearch.Jobs/LINGYUN/Abp/Elasticsearch/Jobs/Localization/ElasticsearchJobsResource.cs
@@ -0,0 +1,8 @@
+﻿using Volo.Abp.Localization;
+
+namespace LINGYUN.Abp.Elasticsearch.Jobs.Localization;
+
+[LocalizationResourceName("ElasticsearchJobs")]
+public class ElasticsearchJobsResource
+{
+}
