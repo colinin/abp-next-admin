889cd8a5b6b4a0f05237041185e7eb97c544242e
colin <colin.in@foxmail.com>
2023-09-08 11:22:38 +0800

    fix(identity): fix issues #853

---
+++ aspnet-core/modules/identity/LINGYUN.Abp.Identity.Domain/LINGYUN/Abp/Identity/IOrganizationUnitRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Identity;
+using Volo.Abp.Specifications;
+
+namespace LINGYUN.Abp.Identity;
+
+public interface IOrganizationUnitRepository : Volo.Abp.Identity.IOrganizationUnitRepository
+{
+    Task<int> GetCountAsync(
+        ISpecification<OrganizationUnit> specification,
+        CancellationToken cancellationToken = default);
+
+    Task<List<OrganizationUnit>> GetListAsync(
+        ISpecification<OrganizationUnit> specification,
+        string sorting = nameof(OrganizationUnit.Code),
+        int maxResultCount = 10,
+        int skipCount = 0,
+        bool includeDetails = false,
+        CancellationToken cancellationToken = default);
+}
---
+++ aspnet-core/modules/identity/LINGYUN.Abp.Identity.Domain/System/Linq/Expressions/ExpressionFuncExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Volo.Abp.Specifications;
+
+namespace System.Linq.Expressions;
+
+public static class ExpressionFuncExtensions
+{
+    public static Expression<Func<T, bool>> AndIf<T>(
+        this Expression<Func<T, bool>> first,
+        bool condition,
+        Expression<Func<T, bool>> second)
+    {
+        if (condition)
+        {
+            return ExpressionFuncExtender.And(first, second);
+        }
+
+        return first;
+    }
+
+    public static Expression<Func<T, bool>> OrIf<T>(
+        this Expression<Func<T, bool>> first,
+        bool condition,
+        Expression<Func<T, bool>> second)
+    {
+        if (condition)
+        {
+            return ExpressionFuncExtender.Or(first, second);
+        }
+
+        return first;
+    }
+}
