3077d5c82c3111c45adbde22a5d200cb5d7482b2
colin <colin.in@foxmail.com>
2023-10-30 15:19:29 +0800

    feat(settings): merge settings api to cancel aggregation at the gateway layer.

--- aspnet-core/framework/settings/LINGYUN.Abp.SettingManagement.Application.Contracts/LINGYUN/Abp/SettingManagement/AbpSettingManagementApplicationContractsModule.cs
+++ aspnet-core/framework/settings/LINGYUN.Abp.SettingManagement.Application.Contracts/LINGYUN/Abp/SettingManagement/AbpSettingManagementApplicationContractsModule.cs
@@ -1,4 +1,7 @@
-﻿using Volo.Abp.Application;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using Volo.Abp.Application;
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
 using Volo.Abp.SettingManagement;
@@ -11,6 +14,11 @@ namespace LINGYUN.Abp.SettingManagement
     [DependsOn(typeof(AbpSettingManagementDomainSharedModule))]
     public class AbpSettingManagementApplicationContractsModule : AbpModule
     {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            AutoAddSettingProviders(context.Services);
+        }
+
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             Configure<AbpVirtualFileSystemOptions>(options =>
@@ -23,6 +31,32 @@ namespace LINGYUN.Abp.SettingManagement
                 options.Resources
                 .Get<AbpSettingManagementResource>()
                 .AddVirtualJson("/LINGYUN/Abp/SettingManagement/Localization/ApplicationContracts");
+            });
+        }
+
+        private static void AutoAddSettingProviders(IServiceCollection services)
+        {
+            var userSettingProviders = new List<Type>();
+            var globalSettingProviders = new List<Type>();
+
+            services.OnRegistered(context =>
+            {
+                if (typeof(IUserSettingAppService).IsAssignableFrom(context.ImplementationType) &&
+                    context.ImplementationType.Name.EndsWith("AppService"))
+                {
+                    userSettingProviders.Add(context.ImplementationType);
+                }
+                if (typeof(IReadonlySettingAppService).IsAssignableFrom(context.ImplementationType) &&
+                    context.ImplementationType.Name.EndsWith("AppService"))
+                {
+                    globalSettingProviders.Add(context.ImplementationType);
+                }
+            });
+
+            services.Configure<SettingManagementMergeOptions>(options =>
+            {
+                options.UserSettingProviders.AddIfNotContains(userSettingProviders);
+                options.GlobalSettingProviders.AddIfNotContains(globalSettingProviders);
             });
         }
     }
