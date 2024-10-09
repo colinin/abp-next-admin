39c0ce7253c705c393e1b762062fecee7d374e35
colin <colin.in@foxmail.com>
2024-10-09 15:02:32 +0800

    feat(data-protection): 增加客户端数据规则主体

--- aspnet-core/framework/data-protection/LINGYUN.Abp.DataProtection.Abstractions/LINGYUN/Abp/DataProtection/DataAccessFilterOperate.cs
+++ aspnet-core/framework/data-protection/LINGYUN.Abp.DataProtection.Abstractions/LINGYUN/Abp/DataProtection/DataAccessFilterOperate.cs
@@ -7,63 +7,53 @@ namespace LINGYUN.Abp.DataProtection;
 public enum DataAccessFilterOperate
 {
     /// <summary>
-    /// 且
-    /// </summary>
-    [Description("且")]
-    And = 1,
-    /// <summary>
-    /// 或
-    /// </summary>
-    [Description("或")]
-    Or = 2,
-    /// <summary>
     /// 等于
     /// </summary>
     [Description("等于")]
-    Equal = 3,
+    Equal = 1,
     /// <summary>
     /// 不等于
     /// </summary>
     [Description("不等于")]
-    NotEqual = 4,
+    NotEqual = 2,
     /// <summary>
     /// 小于
     /// </summary>
     [Description("小于")]
-    Less = 5,
+    Less = 3,
     /// <summary>
     /// 小于或等于
     /// </summary>
     [Description("小于等于")]
-    LessOrEqual = 6,
+    LessOrEqual = 4,
     /// <summary>
     /// 大于
     /// </summary>
     [Description("大于")]
-    Greater = 7,
+    Greater = 5,
     /// <summary>
     /// 大于或等于
     /// </summary>
     [Description("大于等于")]
-    GreaterOrEqual = 8,
+    GreaterOrEqual = 6,
     /// <summary>
     /// 左包含
     /// </summary>
     [Description("左包含")]
-    StartsWith = 9,
+    StartsWith = 7,
     /// <summary>
     /// 右包含
     /// </summary>
     [Description("右包含")]
-    EndsWith = 10,
+    EndsWith = 8,
     /// <summary>
     /// 包含
     /// </summary>
     [Description("包含")]
-    Contains = 11,
+    Contains = 9,
     /// <summary>
     /// 不包含
     /// </summary>
     [Description("不包含")]
-    NotContains = 12,
+    NotContains = 10,
 }
---
+++ aspnet-core/framework/data-protection/LINGYUN.Abp.DataProtection/LINGYUN/Abp/DataProtection/Subjects/DataAccessClientIdContributor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Clients;
+
+namespace LINGYUN.Abp.DataProtection.Subjects;
+public class DataAccessClientIdContributor : IDataAccessSubjectContributor
+{
+    public string Name => ClientPermissionValueProvider.ProviderName;
+
+    public virtual List<string> GetAllowProperties(DataAccessSubjectContributorContext context)
+    {
+        var allowProperties = new List<string>();
+        var currentClient = context.ServiceProvider.GetRequiredService<ICurrentClient>();
+        if (currentClient.IsAuthenticated)
+        {
+            var resourceStore = context.ServiceProvider.GetRequiredService<IDataProtectedResourceStore>();
+            var resource = resourceStore.Get(Name, currentClient.Id, context.EntityTypeFullName, context.Operation);
+            if (resource?.AllowProperties.Any() == true)
+            {
+                allowProperties.AddIfNotContains(resource.AllowProperties);
+            }
+        }
+        return allowProperties;
+    }
+
+    public virtual List<DataAccessFilterGroup> GetFilterGroups(DataAccessSubjectContributorContext context)
+    {
+        var groups = new List<DataAccessFilterGroup>();
+        var currentClient = context.ServiceProvider.GetRequiredService<ICurrentClient>();
+        if (currentClient.IsAuthenticated)
+        {
+            var resourceStore = context.ServiceProvider.GetRequiredService<IDataProtectedResourceStore>();
+            var resource = resourceStore.Get(Name, currentClient.Id, context.EntityTypeFullName, context.Operation);
+            if (resource?.FilterGroup != null)
+            {
+                groups.Add(resource.FilterGroup);
+            }
+        }
+        return groups;
+    }
+}
