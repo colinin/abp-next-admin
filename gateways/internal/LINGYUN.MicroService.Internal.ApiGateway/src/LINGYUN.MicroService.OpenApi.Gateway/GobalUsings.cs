365ca04833e45e84dd76c92581ec2a2cac497b42
colin <colin.in@foxmail.com>
2024-11-22 09:35:03 +0800

    feat(open-api): api signature is passed from the request header

---
+++ gateways/internal/LINGYUN.MicroService.Internal.ApiGateway/src/LINGYUN.MicroService.OpenApi.Gateway/GobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using LINGYUN.MicroService.OpenApi.Gateway;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Serilog;
+global using System;
