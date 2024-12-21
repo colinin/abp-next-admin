20fc80c827eff59d7aa58cf6c491083c67d27f59
colin <colin.in@foxmail.com>
2024-12-21 10:16:55 +0800

    feat(openiddict): 增加applications权限常量.

---
+++ apps/vben5/packages/@abp/openiddict/src/constants/permissions.ts
@@ -0,0 +1,16 @@
+/** 用户权限 */
+export const IdentityUserPermissions = {
+  /** 新增 */
+  Create: 'AbpOpenIddict.Applications.Create',
+  Default: 'AbpOpenIddict.Applications',
+  /** 删除 */
+  Delete: 'AbpOpenIddict.Applications.Delete',
+  /** 管理功能 */
+  ManageFeatures: 'AbpOpenIddict.Applications.ManageFeatures',
+  /** 管理权限 */
+  ManagePermissions: 'AbpOpenIddict.Applications.ManagePermissions',
+  /** 管理密钥 */
+  ManageSecret: 'AbpOpenIddict.Applications.ManageSecret',
+  /** 更新 */
+  Update: 'AbpOpenIddict.Applications.Update',
+};
