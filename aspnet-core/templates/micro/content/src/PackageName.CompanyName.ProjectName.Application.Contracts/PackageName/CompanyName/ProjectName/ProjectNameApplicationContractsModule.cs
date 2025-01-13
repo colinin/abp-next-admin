eb442130d851840cae6ce58cf2bb6da6b2750d97
colin <colin.in@foxmail.com>
2025-01-13 16:33:25 +0800

    :arrow_up: feat: Upgrade abp framework to 8.3.4

--- aspnet-core/templates/micro/content/src/PackageName.CompanyName.ProjectName.Application.Contracts/PackageName/CompanyName/ProjectName/ProjectNameApplicationContractsModule.cs
+++ aspnet-core/templates/micro/content/src/PackageName.CompanyName.ProjectName.Application.Contracts/PackageName/CompanyName/ProjectName/ProjectNameApplicationContractsModule.cs
@@ -1,13 +1,11 @@
 ﻿using LINGYUN.Abp.Dynamic.Queryable;
 using Volo.Abp.Application;
 using Volo.Abp.Authorization;
-using Volo.Abp.Features;
 using Volo.Abp.Modularity;
 
 namespace PackageName.CompanyName.ProjectName;
 
 [DependsOn(
-    typeof(AbpFeaturesModule),
     typeof(AbpAuthorizationModule),
     typeof(AbpDddApplicationContractsModule),
     typeof(AbpDynamicQueryableApplicationContractsModule),
