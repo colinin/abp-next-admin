479f2bdecef0c782299f6b4b179784ba860b2b1d
colin <colin.in@foxmail.com>
2025-03-03 21:02:20 +0800

    feat(gdpr): 增加客户端代理模块

---
+++ aspnet-core/modules/gdpr/LINGYUN.Abp.Gdpr.HttpApi.Client/ClientProxies/LINGYUN/Abp/Gdpr/GdprRequestClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of GdprRequestClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace LINGYUN.Abp.Gdpr;
+
+public partial class GdprRequestClientProxy
+{
+}
