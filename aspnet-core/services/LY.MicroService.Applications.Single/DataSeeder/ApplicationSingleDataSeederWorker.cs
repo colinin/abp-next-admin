767f84d5bdc8b94a0d13b7bcad2f24dddf540fb8
tanshanli <tanshanli@qq.com>
2025-03-31 19:35:43 +0800

    fix:#1157 种子数据没运行导致单体项目菜单无权限访问

---
+++ aspnet-core/services/LY.MicroService.Applications.Single/DataSeeder/ApplicationSingleDataSeederWorker.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp.Data;
+
+namespace LY.MicroService.Applications.Single.DataSeeder;
+
+public class ApplicationSingleDataSeederWorker : BackgroundService
+{
+    protected IDataSeeder DataSeeder { get; }
+
+    public ApplicationSingleDataSeederWorker(IDataSeeder dataSeeder)
+    {
+        DataSeeder = dataSeeder;
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await DataSeeder.SeedAsync();
+    }
+}
