7c4391e3ea2fbf2cefc34c3e297bb0d96b8204c3
colin <colin.in@foxmail.com>
2025-04-09 17:22:56 +0800

    fix(data-protected): 增加多租户模块依赖

--- aspnet-core/framework/data-protection/LINGYUN.Abp.DataProtection.Abstractions/LINGYUN/Abp/DataProtection/AbpDataProtectionAbstractionsModule.cs
+++ aspnet-core/framework/data-protection/LINGYUN.Abp.DataProtection.Abstractions/LINGYUN/Abp/DataProtection/AbpDataProtectionAbstractionsModule.cs
@@ -2,12 +2,14 @@
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
 using Volo.Abp.ObjectExtending;
 using Volo.Abp.VirtualFileSystem;
 
 namespace LINGYUN.Abp.DataProtection;
 
 [DependsOn(typeof(AbpLocalizationModule))]
+[DependsOn(typeof(AbpMultiTenancyModule))]
 [DependsOn(typeof(AbpObjectExtendingModule))]
 public class AbpDataProtectionAbstractionsModule : AbpModule
 {
