8f60e780486d841278ad74cf02c4fe46595c5524
colin <colin.in@foxmail.com>
2025-06-21 18:31:50 +0800

    feat(exporter): Add a PDF export module
    - Add the integration of [LibreOffice](https://www.libreoffice.org) PDF export
    - Add the integration of [Spire.XLS](https://www.e-iceblue.cn/spirexls/spire-xls-for-net-program-guide-content.html) PDF export

---
+++ aspnet-core/framework/exporter/LINGYUN.Abp.Exporter.Pdf.LibreOffice/LINGYUN/Abp/Exporter/Pdf/LibreOffice/AbpExporterPdfLibreOfficeModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace LINGYUN.Abp.Exporter.Pdf.LibreOffice;
+
+[DependsOn(typeof(AbpExporterPdfModule))]
+public class AbpExporterPdfLibreOfficeModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        if (!LibreOfficeCommands.IsLibreOffliceInstalled())
+        {
+            throw new Volo.Abp.AbpInitializationException("Libreoffice not installed in the current operation environment of the host, please refer to the document after installation using ` AbpExporterPdfLibreOfficeModule ` module.");
+        }
+        context.Services.AddTransient<IExcelToPdfProvider, LibreOfficeExcelToPdfProvider>();
+    }
+}
---
+++ aspnet-core/framework/exporter/LINGYUN.Abp.Exporter.Pdf/LINGYUN/Abp/Exporter/Pdf/NullExcelToPdfProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace LINGYUN.Abp.Exporter.Pdf;
+
+[Dependency(TryRegister = true)]
+public class NullExcelToPdfProvider : IExcelToPdfProvider, ISingletonDependency
+{
+    private readonly static Stream _nullStreamCache = Stream.Null;
+    public virtual Task<Stream> ParseAsync(Stream excelStream, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_nullStreamCache);
+    }
+}
