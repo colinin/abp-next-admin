e817431fe7e30b035bead5c124b9df539753d57f
colin <colin.in@foxmail.com>
2025-06-21 18:56:09 +0800

    fix(exporter): fix unit tests

--- aspnet-core/framework/exporter/LINGYUN.Abp.Exporter.Pdf.LibreOffice/LINGYUN/Abp/Exporter/Pdf/LibreOffice/AbpExporterPdfLibreOfficeModule.cs
+++ aspnet-core/framework/exporter/LINGYUN.Abp.Exporter.Pdf.LibreOffice/LINGYUN/Abp/Exporter/Pdf/LibreOffice/AbpExporterPdfLibreOfficeModule.cs
@@ -8,6 +8,11 @@ public class AbpExporterPdfLibreOfficeModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        // 解决单元测试问题
+        if (context.Services.IsAdded<LibreOfficeTestEnvironment>())
+        {
+            return;
+        }
         if (!LibreOfficeCommands.IsLibreOffliceInstalled())
         {
             throw new Volo.Abp.AbpInitializationException("Libreoffice not installed in the current operation environment of the host, please refer to the document after installation using ` AbpExporterPdfLibreOfficeModule ` module.");
--- aspnet-core/framework/exporter/LINGYUN.Abp.Exporter.Pdf/LINGYUN/Abp/Exporter/Pdf/OriginalExcelToPdfProvider.cs
+++ aspnet-core/framework/exporter/LINGYUN.Abp.Exporter.Pdf/LINGYUN/Abp/Exporter/Pdf/OriginalExcelToPdfProvider.cs
@@ -6,11 +6,10 @@ using Volo.Abp.DependencyInjection;
 namespace LINGYUN.Abp.Exporter.Pdf;
 
 [Dependency(TryRegister = true)]
-public class NullExcelToPdfProvider : IExcelToPdfProvider, ISingletonDependency
+public class OriginalExcelToPdfProvider : IExcelToPdfProvider, ISingletonDependency
 {
-    private readonly static Stream _nullStreamCache = Stream.Null;
     public virtual Task<Stream> ParseAsync(Stream excelStream, CancellationToken cancellationToken = default)
     {
-        return Task.FromResult(_nullStreamCache);
+        return Task.FromResult(excelStream);
     }
 }
