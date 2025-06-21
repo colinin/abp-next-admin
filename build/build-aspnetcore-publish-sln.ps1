1c42cc2e222b10111fe265a56cd2aa380e9b76a1
colin <colin.in@foxmail.com>
2025-06-21 08:23:37 +0800

    fix(build): Fix the path for publishing nuget packages
    - update `common.props`
    - use `dotnet pack xxx`

--- build/build-aspnetcore-publish-sln.ps1
+++ build/build-aspnetcore-publish-sln.ps1
@@ -2,8 +2,8 @@
 
 # Build all solutions
 foreach ($solution in $solutionArray) {  
-    #dotnet pack -c Release --include-source --include-symbols $solution.File --no-cache
-    dotnet build -c Release $solution.File --no-cache
+    dotnet pack -c Release --include-source --include-symbols $solution.File --no-cache
+    # dotnet build -c Release $solution.File --no-cache
 }
 
 Set-Location $rootFolder
