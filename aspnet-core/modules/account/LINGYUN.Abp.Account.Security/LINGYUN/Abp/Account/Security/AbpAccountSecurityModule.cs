015e5acad28502f17a77c83eef58769710281539
colin <colin.in@foxmail.com>
2025-11-08 11:38:07 +0800

    feat(account): add `AbpAccountSecurityModule`

--- aspnet-core/modules/account/LINGYUN.Abp.Account.Security/LINGYUN/Abp/Account/Security/AbpAccountSecurityModule.cs
+++ aspnet-core/modules/account/LINGYUN.Abp.Account.Security/LINGYUN/Abp/Account/Security/AbpAccountSecurityModule.cs
@@ -1,31 +1,31 @@
-﻿using LINGYUN.Abp.Account.Emailing.Localization;
-using System;
+﻿using LINGYUN.Abp.Account.Security.Localization;
 using Volo.Abp.Emailing;
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.Sms;
 using Volo.Abp.UI.Navigation;
 using Volo.Abp.VirtualFileSystem;
 
-namespace LINGYUN.Abp.Account.Emailing;
+namespace LINGYUN.Abp.Account.Security;
 
 [DependsOn(
     typeof(AbpEmailingModule),
+    typeof(AbpSmsModule),
     typeof(AbpUiNavigationModule))]
-[Obsolete("This module has been deprecated. Please use AbpAccountSecurityModule.")]
-public class AbpAccountEmailingModule : AbpModule
+public class AbpAccountSecurityModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         Configure<AbpVirtualFileSystemOptions>(options =>
         {
-            options.FileSets.AddEmbedded<AbpAccountEmailingModule>();
+            options.FileSets.AddEmbedded<AbpAccountSecurityModule>();
         });
 
         Configure<AbpLocalizationOptions>(options =>
         {
             options.Resources
-                .Add<AccountEmailingResource>("en")
-                .AddVirtualJson("/LINGYUN/Abp/Account/Emailing/Localization/Resources");
+                .Add<AccountSecurityResource>("en")
+                .AddVirtualJson("/LINGYUN/Abp/Account/Security/Localization/Resources");
         });
     }
 }
