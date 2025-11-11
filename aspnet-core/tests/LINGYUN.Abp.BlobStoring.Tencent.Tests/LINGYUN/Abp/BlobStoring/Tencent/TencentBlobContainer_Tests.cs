0653ebe7e91c6b1160665af85fdfeaf1bf2baeeb
colin <colin.in@foxmail.com>
2025-11-11 11:07:29 +0800

    tests: Add some unit tests

---
+++ aspnet-core/tests/LINGYUN.Abp.BlobStoring.Tencent.Tests/LINGYUN/Abp/BlobStoring/Tencent/TencentBlobContainer_Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace LINGYUN.Abp.BlobStoring.Tencent;
+
+
+//Please set the correct connection string in secrets.json and continue the test.
+public class TencentBlobContainer_Tests : BlobContainer_Tests<AbpBlobStoringTencentTestModule>
+{
+    public TencentBlobContainer_Tests()
+    {
+
+
+    }
+}
