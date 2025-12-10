2af8c6a0bb5328de5af33ba14cdcfdb4d3729119
colin <colin.in@foxmail.com>
2025-12-10 11:13:17 +0800

    feat(tasks): Delete the job status check
    - When deleting an assignment, check whether it has been stopped
    - Add or delete the error code for not stopping the job

--- aspnet-core/modules/task-management/LINGYUN.Abp.TaskManagement.Domain.Shared/LINGYUN/Abp/TaskManagement/TaskManagementErrorCodes.cs
+++ aspnet-core/modules/task-management/LINGYUN.Abp.TaskManagement.Domain.Shared/LINGYUN/Abp/TaskManagement/TaskManagementErrorCodes.cs
@@ -3,9 +3,17 @@
     public static class TaskManagementErrorCodes
     {
         public const string Namespace = "TaskManagement";
-
+        /// <summary>
+        /// 分组 {Group} 中已经存在一个名称为 {Name} 的作业!
+        /// </summary>
         public const string JobNameAlreadyExists = Namespace + ":01000";
-
+        /// <summary>
+        /// 队列没有找到分组 {Group} 中名称为 {Name} 的作业, 请先将作业入队!
+        /// </summary>
         public const string JobNotFoundInQueue = Namespace + ":01001";
+        /// <summary>
+        /// 仅允许删除已停止作业
+        /// </summary>
+        public const string OnlyDeletionOfStopJobsIsAllowed = Namespace + ":01002";
     }
 }
