fa90e2ec1025349b4b43eb7848a13fe92d540a8e
colin <colin.in@foxmail.com>
2026-01-09 08:20:19 +0800

    feat(data-protected): Use Mapperly instead of AutoMapper

--- aspnet-core/modules/data-protection/LINGYUN.Abp.DataProtectionManagement.Domain/LINGYUN/Abp/DataProtectionManagement/AbpDataProtectionManagementDomainModule.cs
+++ aspnet-core/modules/data-protection/LINGYUN.Abp.DataProtectionManagement.Domain/LINGYUN/Abp/DataProtectionManagement/AbpDataProtectionManagementDomainModule.cs
@@ -1,16 +1,14 @@
 ﻿using LINGYUN.Abp.DataProtection;
-using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp.AutoMapper;
-using Volo.Abp.Caching;
 using Volo.Abp.Domain;
 using Volo.Abp.Domain.Entities.Events.Distributed;
+using Volo.Abp.Mapperly;
 using Volo.Abp.Modularity;
 
 namespace LINGYUN.Abp.DataProtectionManagement;
 
 [DependsOn(
-    typeof(AbpAutoMapperModule),
+    typeof(AbpMapperlyModule),
     typeof(AbpDddDomainModule),
     typeof(AbpDataProtectionModule),
     typeof(AbpDataProtectionManagementDomainSharedModule)
@@ -19,12 +17,7 @@ public class AbpDataProtectionManagementDomainModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        context.Services.AddAutoMapperObjectMapper<AbpDataProtectionManagementDomainModule>();
-
-        Configure<AbpAutoMapperOptions>(options =>
-        {
-            options.AddProfile<DataProtectionManagementDomainMappingProfile>(validate: true);
-        });
+        context.Services.AddMapperlyObjectMapper<AbpDataProtectionManagementDomainModule>();
 
         Configure<AbpDistributedEntityEventOptions>(options =>
         {
