3ae8ce0b674baf0dbd945281b5c07d07da91366e
colin <colin.in@foxmail.com>
2026-01-16 11:33:38 +0800

    feat: Add AIManagement localization resource

--- aspnet-core/modules/ai/LINGYUN.Abp.AIManagement.Domain.Shared/LINGYUN/Abp/AIManagement/AbpAIManagementDomainSharedModule.cs
+++ aspnet-core/modules/ai/LINGYUN.Abp.AIManagement.Domain.Shared/LINGYUN/Abp/AIManagement/AbpAIManagementDomainSharedModule.cs
@@ -1,10 +1,25 @@
-﻿using Volo.Abp.Domain;
+﻿using LINGYUN.Abp.AIManagement.Localization;
+using Volo.Abp.Domain;
+using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
 
 namespace LINGYUN.Abp.AIManagement;
 
 [DependsOn(typeof(AbpDddDomainSharedModule))]
 public class AbpAIManagementDomainSharedModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpAIManagementDomainSharedModule>();
+        });
 
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources.Add<AIManagementResource>()
+                .AddVirtualJson("/LINGYUN/Abp/AIManagement/Localization/Resources");
+        });
+    }
 }
