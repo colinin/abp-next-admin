32b9d24b017bc4accb85bf4f188934f3c6289914
colin <colin.in@foxmail.com>
2026-03-26 16:53:41 +0800

    feat(ai): Add Ollama Integration

---
+++ aspnet-core/modules/ai/LINGYUN.Abp.AI.Ollama/LINGYUN/Abp/AI/Ollama/AbpAIOllamaModule.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Modularity;
+
+namespace LINGYUN.Abp.AI.Ollama;
+
+[DependsOn(typeof(AbpAICoreModule))]
+public class AbpAIOllamaModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpAICoreOptions>(options =>
+        {
+            options.ChatClientProviders.Add<OllamaChatClientProvider>();
+
+            options.KernelProviders.Add<OllamaKernelProvider>();
+        });
+    }
+}
