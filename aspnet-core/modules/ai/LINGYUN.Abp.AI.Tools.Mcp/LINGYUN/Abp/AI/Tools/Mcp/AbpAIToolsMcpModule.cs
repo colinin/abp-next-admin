8c49ca611956171e622ebf3a695acbeea3c1a131
colin <colin.in@foxmail.com>
2026-03-27 14:37:44 +0800

    feat(ai): Increase the localization of the Mcp tool

--- aspnet-core/modules/ai/LINGYUN.Abp.AI.Tools.Mcp/LINGYUN/Abp/AI/Tools/Mcp/AbpAIToolsMcpModule.cs
+++ aspnet-core/modules/ai/LINGYUN.Abp.AI.Tools.Mcp/LINGYUN/Abp/AI/Tools/Mcp/AbpAIToolsMcpModule.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using LINGYUN.Abp.AI.Localization;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Http.Client;
+using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
 
 namespace LINGYUN.Abp.AI.Tools.Mcp;
 
@@ -13,10 +16,22 @@ public class AbpAIToolsMcpModule : AbpModule
     {
         context.Services.AddMcpAIToolClient();
 
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpAIToolsMcpModule>();
+        });
+
         Configure<AbpAIToolsOptions>(options =>
         {
             // Mcp工具
             options.AIToolProviders.Add<McpAIToolProvider>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Get<AbpAIResource>()
+                .AddVirtualJson("/LINGYUN/Abp/AI/Tools/Mcp/Localization/Resources");
         });
     }
 }
