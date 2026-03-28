86998d9e765afc1ba3a8fd0f7d032aad59a13eb1
colin <colin.in@foxmail.com>
2026-03-28 09:11:47 +0800

    feat(ai): Adjust the length of the description field for the tool

--- aspnet-core/modules/ai/LINGYUN.Abp.AIManagement.Domain.Shared/LINGYUN/Abp/AIManagement/Tools/AIToolDefinitionRecordConsts.cs
+++ aspnet-core/modules/ai/LINGYUN.Abp.AIManagement.Domain.Shared/LINGYUN/Abp/AIManagement/Tools/AIToolDefinitionRecordConsts.cs
@@ -3,6 +3,6 @@ public static class AIToolDefinitionRecordConsts
 {
     public static int MaxNameLength { get; set; } = 64;
     public static int MaxProviderLength { get; set; } = 20;
-    public static int MaxDescriptionLength { get; set; } = 128;
+    public static int MaxDescriptionLength { get; set; } = 1024;
     public static int MaxStateCheckersLength { get; set; } = 256;
 }
