1f8468e51c403f3ed1c24e190322f2b292162bfa
colin <colin.in@foxmail.com>
2026-06-22 14:06:19 +0800

    feat: Optimize dynamic configuration related aspects

---
+++ aspnet-core/aspire/LINGYUN.Abp.MicroService.AdminService/Overrides/MergeTemplateDefinitionManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Immutable;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.TextTemplating;
+
+namespace LINGYUN.Abp.MicroService.AdminService.Overrides;
+
+
+[Dependency(ReplaceServices = true)]
+public class MergeTemplateDefinitionManager : TemplateDefinitionManager
+{
+    public MergeTemplateDefinitionManager(
+        IStaticTemplateDefinitionStore staticStore,
+        IDynamicTemplateDefinitionStore dynamicStore)
+        : base(staticStore, dynamicStore)
+    {
+    }
+    public async override Task<IReadOnlyList<TemplateDefinition>> GetAllAsync()
+    {
+        var staticTemplates = await StaticStore.GetAllAsync();
+        var dynamicTemplates = await DynamicStore.GetAllAsync();
+
+        var mergedTemplates = new Dictionary<string, TemplateDefinition>();
+
+        foreach (var staticTemplate in staticTemplates)
+        {
+            mergedTemplates[staticTemplate.Name] = staticTemplate;
+        }
+
+        foreach (var dynamicTemplate in dynamicTemplates)
+        {
+            if (mergedTemplates.TryGetValue(dynamicTemplate.Name, out var existingTemplate))
+            {
+                MergeTemplate(existingTemplate, dynamicTemplate);
+            }
+            else
+            {
+                mergedTemplates[dynamicTemplate.Name] = dynamicTemplate;
+            }
+        }
+
+        return mergedTemplates.Values.ToImmutableList();
+    }
+
+    private static TemplateDefinition MergeTemplate(TemplateDefinition staticTemplate, TemplateDefinition dynamicTemplate)
+    {
+        var localizationResourceName = dynamicTemplate.LocalizationResourceName ?? staticTemplate.LocalizationResourceName;
+        var defaultCultureName = dynamicTemplate.DefaultCultureName ?? staticTemplate.DefaultCultureName;
+        var displayName = dynamicTemplate.DisplayName ?? staticTemplate.DisplayName;
+        var isLayout = dynamicTemplate.IsLayout || staticTemplate.IsLayout;
+        var layout = dynamicTemplate.Layout ?? staticTemplate.Layout;
+
+        var mergedTemplate = new TemplateDefinition(
+            staticTemplate.Name,
+            localizationResourceName,
+            displayName,
+            isLayout,
+            layout,
+            defaultCultureName
+        );
+
+        foreach (var property in staticTemplate.Properties)
+        {
+            mergedTemplate.Properties[property.Key] = property.Value;
+        }
+
+        foreach (var property in dynamicTemplate.Properties)
+        {
+            mergedTemplate.Properties[property.Key] = property.Value;
+        }
+
+        if (!string.IsNullOrEmpty(dynamicTemplate.RenderEngine))
+        {
+            mergedTemplate.RenderEngine = dynamicTemplate.RenderEngine;
+        }
+        else if (!string.IsNullOrEmpty(staticTemplate.RenderEngine))
+        {
+            mergedTemplate.RenderEngine = staticTemplate.RenderEngine;
+        }
+
+        mergedTemplate.IsInlineLocalized = dynamicTemplate.IsInlineLocalized || staticTemplate.IsInlineLocalized;
+
+        return mergedTemplate;
+    }
+}
---
+++ aspnet-core/services/LY.MicroService.BackendAdmin.HttpApi.Host/Overrides/MergePermissionDefinitionManager.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+
+namespace LY.MicroService.BackendAdmin.Overrides;
+
+[Dependency(ReplaceServices = true)]
+public class MergePermissionDefinitionManager : PermissionDefinitionManager
+{
+    private readonly IStaticPermissionDefinitionStore _staticStore;
+    private readonly IDynamicPermissionDefinitionStore _dynamicStore;
+
+    public MergePermissionDefinitionManager(
+        IStaticPermissionDefinitionStore staticStore, 
+        IDynamicPermissionDefinitionStore dynamicStore) : base(staticStore, dynamicStore)
+    {
+        _staticStore = staticStore;
+        _dynamicStore = dynamicStore;
+    }
+
+    public async override Task<IReadOnlyList<PermissionGroupDefinition>> GetGroupsAsync()
+    {
+        var staticGroups = await _staticStore.GetGroupsAsync();
+        var dynamicGroups = await _dynamicStore.GetGroupsAsync();
+
+        var mergedGroups = new Dictionary<string, PermissionGroupDefinition>();
+
+        foreach (var staticGroup in staticGroups)
+        {
+            mergedGroups[staticGroup.Name] = staticGroup;
+        }
+
+        foreach (var dynamicGroup in dynamicGroups)
+        {
+            if (mergedGroups.TryGetValue(dynamicGroup.Name, out var existingGroup))
+            {
+                MergeGroupPermissions(existingGroup, dynamicGroup);
+            }
+            else
+            {
+                mergedGroups[dynamicGroup.Name] = dynamicGroup;
+            }
+        }
+
+        return mergedGroups.Values.ToImmutableList();
+    }
+
+    private static void MergeGroupPermissions(PermissionGroupDefinition target, PermissionGroupDefinition source)
+    {
+        foreach (var sourcePermission in source.Permissions)
+        {
+            var existingPermission = target.GetPermissionOrNull(sourcePermission.Name);
+
+            if (existingPermission == null)
+            {
+                var newPermission = target.AddPermission(
+                    sourcePermission.Name,
+                    sourcePermission.DisplayName,
+                    sourcePermission.MultiTenancySide,
+                    sourcePermission.IsEnabled
+                );
+
+                CopyPermissionDetails(sourcePermission, newPermission);
+
+                foreach (var child in sourcePermission.Children)
+                {
+                    AddChildPermissionRecursively(newPermission, child);
+                }
+            }
+            else
+            {
+                MergePermissionMetadata(existingPermission, sourcePermission);
+
+                foreach (var sourceChild in sourcePermission.Children)
+                {
+                    MergeChildPermissions(existingPermission, sourceChild);
+                }
+            }
+        }
+    }
+
+    private static void AddChildPermissionRecursively(PermissionDefinition parent, PermissionDefinition sourceChild)
+    {
+        var newChild = parent.AddChild(
+            sourceChild.Name,
+            sourceChild.DisplayName,
+            sourceChild.MultiTenancySide,
+            sourceChild.IsEnabled
+        );
+
+        CopyPermissionDetails(sourceChild, newChild);
+
+        foreach (var grandchild in sourceChild.Children)
+        {
+            AddChildPermissionRecursively(newChild, grandchild);
+        }
+    }
+
+    private static void MergeChildPermissions(PermissionDefinition parent, PermissionDefinition sourceChild)
+    {
+        var existingChild = parent.Children.FirstOrDefault(c => c.Name == sourceChild.Name);
+
+        if (existingChild == null)
+        {
+            var newChild = parent.AddChild(
+                sourceChild.Name,
+                sourceChild.DisplayName,
+                sourceChild.MultiTenancySide,
+                sourceChild.IsEnabled
+            );
+            CopyPermissionDetails(sourceChild, newChild);
+
+            foreach (var grandchild in sourceChild.Children)
+            {
+                AddChildPermissionRecursively(newChild, grandchild);
+            }
+        }
+        else
+        {
+            MergePermissionMetadata(existingChild, sourceChild);
+
+            foreach (var grandchild in sourceChild.Children)
+            {
+                MergeChildPermissions(existingChild, grandchild);
+            }
+        }
+    }
+
+    private static void CopyPermissionDetails(PermissionDefinition source, PermissionDefinition target)
+    {
+        foreach (var property in source.Properties)
+        {
+            target.Properties[property.Key] = property.Value;
+        }
+
+        foreach (var provider in source.Providers)
+        {
+            if (!target.Providers.Contains(provider))
+            {
+                target.Providers.Add(provider);
+            }
+        }
+
+        foreach (var checker in source.StateCheckers)
+        {
+            if (!target.StateCheckers.Contains(checker))
+            {
+                target.StateCheckers.Add(checker);
+            }
+        }
+    }
+
+    private static void MergePermissionMetadata(PermissionDefinition target, PermissionDefinition source)
+    {
+        target.DisplayName = source.DisplayName;
+
+        foreach (var property in source.Properties)
+        {
+            target.Properties[property.Key] = property.Value;
+        }
+
+        foreach (var provider in source.Providers)
+        {
+            if (!target.Providers.Contains(provider))
+            {
+                target.Providers.Add(provider);
+            }
+        }
+
+        foreach (var checker in source.StateCheckers)
+        {
+            if (!target.StateCheckers.Contains(checker))
+            {
+                target.StateCheckers.Add(checker);
+            }
+        }
+
+        if (source.MultiTenancySide != MultiTenancySides.Both)
+        {
+            target.MultiTenancySide |= source.MultiTenancySide;
+        }
+
+        target.IsEnabled = target.IsEnabled || source.IsEnabled;
+    }
+}
