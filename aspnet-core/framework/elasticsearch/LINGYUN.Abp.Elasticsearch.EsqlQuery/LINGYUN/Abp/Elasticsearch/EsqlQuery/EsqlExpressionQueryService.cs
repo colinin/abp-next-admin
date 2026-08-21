ddda353df5fff31d9d411b5a65530e2e120f2985
colin <colin.in@foxmail.com>
2026-08-21 11:58:28 +0800

    feat: Add the ESQL testing module

--- aspnet-core/framework/elasticsearch/LINGYUN.Abp.Elasticsearch.EsqlQuery/LINGYUN/Abp/Elasticsearch/EsqlQuery/EsqlExpressionQueryService.cs
+++ aspnet-core/framework/elasticsearch/LINGYUN.Abp.Elasticsearch.EsqlQuery/LINGYUN/Abp/Elasticsearch/EsqlQuery/EsqlExpressionQueryService.cs
@@ -11,6 +11,15 @@ using Volo.Abp.DependencyInjection;
 
 namespace LINGYUN.Abp.Elasticsearch.EsqlQuery;
 
+/// <summary>
+/// NOTE: The tests for methods like Contains in the collection have failed. Please do not use this interface for collection filtering.
+/// TODO: According to the official documentation, is it feasible? Is the support for nested types not available?
+/// See: https://www.elastic.co/docs/reference/elasticsearch/clients/dotnet/linq-to-esql#linq-esql-filtering
+/// var brands = new[] { "TechCorp", "StyleMax", "HomeBase" };
+/// client.Esql.Query<Product>(q => q.Where(p => brands.Contains(p.Brand)));
+/// → WHERE brand IN ("TechCorp", "StyleMax", "HomeBase")
+/// </summary>
+
 [Dependency(ReplaceServices = true)]
 public class EsqlExpressionQueryService : ExpressionQueryService, ITransientDependency
 {
@@ -29,6 +38,7 @@ public class EsqlExpressionQueryService : ExpressionQueryService, ITransientDepe
         var client = ClientFactory.Create();
 
         return await client.Esql.CreateQuery<TDocument>()
+            .From(indexName)
             .Where(expression)
             .AsEsqlQueryable()
             .CountAsync(cancellationToken);
@@ -42,18 +52,31 @@ public class EsqlExpressionQueryService : ExpressionQueryService, ITransientDepe
         int skipCount = 0,
         Fields? sourceExcludes = null,
         Fields? sourceIncludes = null,
+        object[]? beginMarker = null,
         CancellationToken cancellationToken = default) where TDocument : class
     {
         var client = ClientFactory.Create();
 
-        var query = client.Esql.CreateQuery<TDocument>().Where(expression);
+        var query = client.Esql.CreateQuery<TDocument>()
+            .From(indexName)
+            .Where(expression);
         if (!sorting.IsNullOrWhiteSpace())
         {
             query = query.OrderBy(sorting);
         }
+        if (sourceExcludes != null)
+        {
+            query = query.Drop(sourceExcludes.Select(f => f.Name!).ToArray());
+        }
+        if (sourceIncludes != null)
+        {
+            query = query.Keep(sourceIncludes.Select(f => f.Name!).ToArray());
+        }
 
+        // TODO: 需要构建范围过滤条件,加入到query中以实现分页查询
+        // See: https://www.elastic.co/docs/reference/elasticsearch/clients/dotnet/linq-to-esql#linq-esql-sorting
         return await query
-            .PageBy(skipCount, maxResultCount)
+            .Take(maxResultCount)
             .AsEsqlQueryable()
             .ToListAsync(cancellationToken);
     }
