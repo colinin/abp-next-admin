e9f677e4d32d39e1321fe59138c4ec454907f1b8
colin <colin.in@foxmail.com>
2026-09-09 17:52:55 +0800

    feat: remove structuredlogs bundle

--- aspnet-core/modules/elsa/LINGYUN.Abp.ElsaNext.Studio.Diagnostics.StructuredLogs.Blazor/AbpElsaNextStudioDiagnosticsStructuredLogsBlazorModule.cs
+++ aspnet-core/modules/elsa/LINGYUN.Abp.ElsaNext.Studio.Diagnostics.StructuredLogs.Blazor/AbpElsaNextStudioDiagnosticsStructuredLogsBlazorModule.cs
@@ -4,7 +4,6 @@ using Elsa.Features.Services;
 using Elsa.Studio.Diagnostics.StructuredLogs.Dashboard.Extensions;
 using Elsa.Studio.Diagnostics.StructuredLogs.Extensions;
 using LINGYUN.Abp.ElsaNext.Studio.Blazor;
-using LINGYUN.Abp.ElsaNext.Studio.Diagnostics.StructuredLogs.Blazor.Bundling;
 using LINGYUN.Abp.ElsaNext.Studio.Diagnostics.StructuredLogs.Blazor.Extensions;
 using LINGYUN.Abp.ElsaNext.Studio.Diagnostics.StructuredLogs.Blazor.Navigation;
 using LINGYUN.Abp.ElsaNext.Studio.Translations.Localization;
@@ -12,7 +11,6 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.AspNetCore;
 using Volo.Abp.AspNetCore.Components.Web.Theming.MudBlazor.Routing;
-using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
 using Volo.Abp.UI.Navigation;
@@ -37,16 +35,6 @@ public class AbpElsaNextStudioDiagnosticsStructuredLogsBlazorModule : AbpModule
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         var configuration = context.Configuration.GetSection("Elsa:Studio");
-
-        Configure<AbpBundlingOptions>(options =>
-        {
-            options
-                .StyleBundles
-                .Add(typeof(Elsa.Studio.Diagnostics.StructuredLogs.UI.Pages.StructuredLogs).FullName!, bundle =>
-                {
-                    bundle.AddContributors(typeof(StructuredLogsStyleContributor));
-                });
-        });
 
         Configure<AbpRouterOptions>(options =>
         {
