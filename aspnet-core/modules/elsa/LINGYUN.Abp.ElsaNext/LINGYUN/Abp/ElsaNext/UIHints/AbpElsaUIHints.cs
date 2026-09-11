f85f3914b79063b8967c82c69adffd244c277d3f
colin <colin.in@foxmail.com>
2026-09-11 11:28:08 +0800

    fix: Invalid notification filtering has been fixed

--- aspnet-core/modules/elsa/LINGYUN.Abp.ElsaNext/LINGYUN/Abp/ElsaNext/UIHints/AbpElsaUIHints.cs
+++ aspnet-core/modules/elsa/LINGYUN.Abp.ElsaNext/LINGYUN/Abp/ElsaNext/UIHints/AbpElsaUIHints.cs
@@ -1,7 +1,8 @@
-﻿namespace LINGYUN.Abp.ElsaNext.UIHints;
+namespace LINGYUN.Abp.ElsaNext.UIHints;
 
 public static class AbpElsaUIHints
 {
     public const string TenantPicker = "tenant-picker";
     public const string UserPicker = "user-picker";
+    public const string UserPickerMultiple = "user-picker-multiple";
 }
