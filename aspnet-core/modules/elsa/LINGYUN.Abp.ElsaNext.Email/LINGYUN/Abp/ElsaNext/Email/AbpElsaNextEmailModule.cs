c70fc6bda05b151cfaef4c371144955407315f5c
colin <colin.in@foxmail.com>
2026-09-11 10:15:38 +0800

    feat: Add Email Activities Module

---
+++ aspnet-core/modules/elsa/LINGYUN.Abp.ElsaNext.Email/LINGYUN/Abp/ElsaNext/Email/AbpElsaNextEmailModule.cs
@@ -0,0 +1,38 @@
+﻿using Elsa.Features.Services;
+using LINGYUN.Abp.ElsaNext.Email.Extensions;
+using LINGYUN.Abp.ElsaNext.Localization;
+using Volo.Abp.Emailing;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace LINGYUN.Abp.ElsaNext.Email;
+
+[DependsOn(
+    typeof(AbpEmailingModule),
+    typeof(AbpElsaNextModule))]
+public class AbpElsaNextEmailModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        PreConfigure<IModule>(elsa =>
+        {
+            elsa.UseAbpEmail();
+        });
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpElsaNextEmailModule>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Get<ElsaNextResource>()
+                .AddVirtualJson("/LINGYUN/Abp/ElsaNext/Email/Localization/Resources");
+        });
+    }
+}
---
+++ aspnet-core/modules/elsa/LINGYUN.Abp.ElsaNext.Email/LINGYUN/Abp/ElsaNext/Email/Contracts/IDownloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LINGYUN.Abp.ElsaNext.Email.Contracts;
+/// <summary>
+/// Download files from the Internet.
+/// </summary>
+public interface IDownloader
+{
+    /// <summary>
+    /// Download whatever is returned at the specified URL.
+    /// </summary>
+    Task<HttpResponseMessage> DownloadAsync(Uri url, CancellationToken cancellationToken = default);
+}
