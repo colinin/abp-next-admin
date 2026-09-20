9a7da1beec01af51682858a91f4ead16a63f0955
colin <colin.in@foxmail.com>
2026-09-20 17:57:20 +0800

    feat: Tencent Cloud Captcha Exception Handling

--- aspnet-core/modules/account/LINGYUN.Abp.Account.Web.TencentCaptcha/TencentCaptchaValidator.cs
+++ aspnet-core/modules/account/LINGYUN.Abp.Account.Web.TencentCaptcha/TencentCaptchaValidator.cs
@@ -1,10 +1,13 @@
 ﻿using LINGYUN.Abp.Account.Web.Captcha;
 using LINGYUN.Abp.Tencent.Settings;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using TencentCloud.Captcha.V20190722;
+using TencentCloud.Captcha.V20190722.Models;
+using TencentCloud.Common;
 using Volo.Abp;
 using Volo.Abp.AspNetCore.WebClientInfo;
 using Volo.Abp.Settings;
@@ -45,26 +48,40 @@ public class TencentCaptchaValidator : ICaptchaValidator
             Check.NotNullOrWhiteSpace(appSecretKey, TencentCloudSettingNames.Captcha.AppSecretKey);
 
             var client = new CaptchaClient(
-                new TencentCloud.Common.Credential
+                new Credential
                 {
                     SecretId = secretId,
                     SecretKey = secretKey
                 },
                 "");
 
-            var response = await client.DescribeCaptchaResult(
-                new TencentCloud.Captcha.V20190722.Models.DescribeCaptchaResultRequest
-                {
-                    CaptchaAppId = ulong.Parse(captchaAppId),
-                    AppSecretKey = appSecretKey,
-                    CaptchaType = 9,
-                    Randstr = captchaKeys[0],
-                    Ticket = captchaKeys[1],
-                    UserIp = webClientInfoProvider.ClientIpAddress,
-                });
-            if (response.CaptchaCode == 1)
+            try
             {
-                return true;
+                var response = await client.DescribeCaptchaResult(
+                    new DescribeCaptchaResultRequest
+                    {
+                        CaptchaAppId = ulong.Parse(captchaAppId),
+                        AppSecretKey = appSecretKey,
+                        CaptchaType = 9,
+                        Randstr = captchaKeys[0],
+                        Ticket = captchaKeys[1],
+                        UserIp = webClientInfoProvider.ClientIpAddress,
+                    });
+                if (response.CaptchaCode == 1)
+                {
+                    return true;
+                }
+                context.ServiceProvider
+                    .GetService<ILogger<TencentCaptchaValidator>>()
+                    ?.LogWarning("Tencent Cloud captcha verification failed, RequestId: {requestId}, Error: {code}: {message}",
+                        response.RequestId, response.CaptchaCode, response.CaptchaMsg);
+            }
+            catch (TencentCloudSDKException ex)
+            {
+                context.ServiceProvider
+                    .GetService<ILogger<TencentCaptchaValidator>>()
+                    ?.LogWarning(ex, "Error occurred while invoking Tencent Cloud captcha service, RequestId: {requestId}, Error: {code}: {message}", 
+                        ex.RequestId, ex.ErrorCode, ex.Message);
             }
         }
 
