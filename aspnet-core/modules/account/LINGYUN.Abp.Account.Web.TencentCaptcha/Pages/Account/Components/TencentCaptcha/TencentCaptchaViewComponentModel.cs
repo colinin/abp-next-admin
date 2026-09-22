96396707f5f30a17eb33fdf77ad3d48d4e460f43
colin <colin.in@foxmail.com>
2026-09-22 18:53:56 +0800

    feat: Remove invalid model definitions

--- aspnet-core/modules/account/LINGYUN.Abp.Account.Web.TencentCaptcha/Pages/Account/Components/TencentCaptcha/TencentCaptchaViewComponentModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using LINGYUN.Abp.Account.Web.Pages.Account;
-
-namespace LINGYUN.Abp.Account.Web.TencentCaptcha.Pages.Account.Components.TencentCaptcha;
-
-public class TencentCaptchaViewComponentModel
-{
-    public PasswordLoginInputModel PasswordLoginInput { get; set; } = default!;
-}
